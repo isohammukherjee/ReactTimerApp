7a742264cfa8b2781429b14f82021c590ea905a9
Soham Mukherjee <soham.mukherjee@teks.co.in>
2018-01-15 18:07:32 +0530

    new changes

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,40 @@
+var React = require('react');
+var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
+
+var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        }, {
+          id: 2,
+          text: 'Clean the yard'
+        }, {
+          id: 3,
+          text: 'Leave mail on porch'
+        }, {
+          id: 4,
+          text: 'Play video games'
+        }
+      ]
+    };
+  },
+  handleAddTodo: function (text) {
+    alert('new todo: ' + text);
+  },
+  render: function () {
+    var {todos} = this.state;
+
+    return (
+      <div>
+        <TodoList todos={todos}/>
+        <AddTodo onAddTodo={this.handleAddTodo}/>
+      </div>
+    )
+  }
+});
+
+module.exports = TodoApp;
